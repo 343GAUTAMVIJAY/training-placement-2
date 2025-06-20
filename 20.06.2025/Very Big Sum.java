a0f82581ef368b277ae923f02ffb6e6118402739
343GAUTAMVIJAY <147785180+343GAUTAMVIJAY@users.noreply.github.com>
2025-06-20 21:09:06 +0530

    Create Very Big Sum.java

---
+++ 20.06.2025/Very Big Sum.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Solution {
+    public static long aVeryBigSum(List<Long> ar) {
+        long sum = 0;
+        for (long num : ar) sum += num;
+        return sum;
+    }
+}
