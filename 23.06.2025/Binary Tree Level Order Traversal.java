94385311cfb42aa8edd5ea72c66e684be5437ecf
343GAUTAMVIJAY <147785180+343GAUTAMVIJAY@users.noreply.github.com>
2025-06-23 20:19:45 +0530

    Create Binary Tree Level Order Traversal.java

---
+++ 23.06.2025/Binary Tree Level Order Traversal.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root == null) return result;
+        q.offer(root);
+        while(!q.isEmpty()){
+            int level = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0; i<level; i++){
+                if(q.peek().left != null) q.offer(q.peek().left);
+                if(q.peek().right != null) q.offer(q.peek().right);
+                list.add(q.poll().val);
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
