7d623e41331f28b8c8d604be9cbb3a5108b09347
343GAUTAMVIJAY <147785180+343GAUTAMVIJAY@users.noreply.github.com>
2025-06-30 11:35:27 +0530

    Create random_quote_generator.py

---
+++ 29.06.2025/random_quote_generator.py
@@ -0,0 +1,8 @@
+import random
+
+quotes = [
+    "Be the change you wish to see. - Gandhi",
+    "Stay hungry, stay foolish. - Steve Jobs",
+    "Think different. - Apple"
+]
+print(random.choice(quotes))
